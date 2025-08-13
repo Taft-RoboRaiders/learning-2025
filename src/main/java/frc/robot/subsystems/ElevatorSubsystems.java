35ba9e299715a230f547e77935970063f71b5979
coco3534 <curtkriscody@gmail.com>
2025-08-13 00:05:50 -0500

    Update ElevatorSubsystems.java

--- src/main/java/frc/robot/subsystems/ElevatorSubsystems.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystems.java
@@ -5,6 +5,8 @@ import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
 
@@ -13,12 +15,20 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SmartMotionConfig;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import yams.gearing.GearBox;
 import yams.mechanisms.SmartMechanism;
+import yams.mechanisms.config.ElevatorConfig;
+import yams.mechanisms.positional.Elevator;
+import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
 
 public class ElevatorSubsystems extends SubsystemBase {
 
@@ -38,6 +48,45 @@ SmartMotorControllerConfig motorConfig = new SmartMotorControllerConfig(this)
 .withClosedLoopRampRate(Seconds.of(0.25))
 .withOpenLoopRampRate(Seconds.of(0.75))
 .withFeedforward(new ElevatorFeedforward(0, 0, 0))
-.withControlMode(C)
+.withControlMode(ControlMode.OPEN_LOOP);
 
+private final SmartMotorController  motor = new SparkWrapper(elevatorL, DCMotor.getNEO(1), motorConfig);
+
+private final ElevatorConfig        m_config    = new ElevatorConfig(motor)
+.withStartingHeight(Meters.of(0))
+.withHardLimits(Meters.of(0), Meters.of(3))
+.withTelemetry("Elevator", TelemetryVerbosity.HIGH)
+.withMass(Pounds.of(16));
+
+private final Elevator          elevator = new Elevator(m_config);
+
+  public ElevatorSubsystems()
+  {
+
+  }
+
+  public void periodic()
+  {
+    elevator.updateTelemetry();
+  }
+
+  public void simulationPeriodic()
+  {
+    elevator.simIterate();
+  }
+
+  public Command elevCmd(double dutycycle)
+  {
+    return elevator.set(dutycycle);
+  }
+
+  public Command setHeight(Distance height)
+  {
+    return elevator.setHeight(height);
+  }
+
+  public Command sysId()
+  {
+    return elevator.sysId(Volts.of(12), Volts.of(12).per(Second), Second.of(30));
+  }
 }
